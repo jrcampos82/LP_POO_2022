1965bef899219bae1cf4d316b263395877eea3bc
Beto <jrcampos82@gmail.com>
2022-08-17 21:32:08 -0400

    upd aula 17 agosto

--- src/poo/ex_heranca/Livro.java
+++ src/poo/ex_heranca/Livro.java
@@ -1,6 +1,7 @@
 package poo.ex_heranca;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Livro {
   private String titulo;
@@ -37,6 +38,48 @@ public class Livro {
 
   @Override
   public String toString() {
-    return "Título: " + this.titulo;
+    return (
+      "Título\t" +
+      this.titulo +
+      "\nEditora\t" +
+      this.editora +
+      "\nData Lanc:\t" +
+      this.dtLancamente
+      //DateTimeFormatter
+    );
+  }
+
+  public static Livro cadastrarLivro() {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Digite o título do livro: ");
+    String titulo = in.next();
+    if (titulo.length() < 2) {
+      System.out.println("O título deve ter ao menos 2 caracteres!");
+      return null;
+    }
+    Livro novoLivro = new Livro(titulo);
+    System.out.print("Digite a editora do livro: ");
+    novoLivro.setEditora(in.next());
+
+    System.out.print("Digite a data de lançamento (dd/mm/yyyy): ");
+    String data = in.next();
+    String dtLanc[] = data.split("/");
+
+    if (dtLanc.length != 3) {
+      System.out.println("Digite a data corretamente");
+
+      return null;
+    }
+
+    int dd = Integer.parseInt(dtLanc[0]);
+    int m = Integer.parseInt(dtLanc[1]);
+    int y = Integer.parseInt(dtLanc[2]);
+
+    LocalDate d = LocalDate.of(y, m, dd);
+
+    novoLivro.setDtLancamente(d);
+
+    //in.close();
+    return novoLivro;
   }
 }
--- src/poo/ex_heranca/SistemaBiblioteca.java
+++ src/poo/ex_heranca/SistemaBiblioteca.java
@@ -1,26 +1,70 @@
 package poo.ex_heranca;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SistemaBiblioteca {
+  private static final String[] opcoes = {
+    "| ----------- +++ Sistema biblioteca +++ ----------- |",
+    "|                 Escolha um opção:                  |",
+    "| -------------------------------------------------- | ",
+    "| 1 - Cadastrar Livro                                |",
+    "| 2 - Cadastrar Funcionario                          |",
+    "| 3 - Cadastrar Estudante                            |",
+    "| 4 - Cadastrar Autor                                |",
+    "| 5 - Realizar Emprestimo                            |",
+    "| 6 - Receber Emprestimo                             |",
+    "| 0 - Sair                                           |",
+    "| -------------------------------------------------- |",
+  };
 
-  public static void main(String[] args) {
-    int op = 0;
+  private static List<Livro> livros = new ArrayList<Livro>();
+  private static List<Autor> autores = new ArrayList<Autor>();
 
-    List<Funcionario> funcionarios = new ArrayList<Funcionario>();
-    List<Autor> autores = new ArrayList<Autor>();
-    List<Livro> livros = new ArrayList<Livro>();
+  private static Scanner buffer = new Scanner(System.in);
 
-    do {
-      System.out.println(" +++ Sistema biblioteca +++");
-      System.out.println("Escolha um opção: ");
-      System.out.println("1 - Cadastrar Livro");
-      System.out.println("2 - Cadastrar Funcionario");
-      System.out.println("3 - Cadastrar Estudante");
-      System.out.println("4- Cadastrar Autor");
-      System.out.println("5 - Realizar Emprestimo");
-      System.out.println("6 - Receber Emprestimo");
-    } while (op != 0);
+  public static void main(String[] args) throws IOException {
+    int op = 1;
+    while (op != 0) {
+      printMenu();
+      System.out.println("\n");
+      op = buffer.nextInt();
+      escolha(op);
+      System.in.read(); // System("pause")
+    }
   }
+
+  //----------------------------------------------------------------
+  private static void printMenu() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    for (String op : opcoes) {
+      System.out.println(op);
+    }
+  }
+
+  //----------------------------------------------------------------
+  private static void escolha(int op) {
+    switch (op) {
+      // cadastrar livro
+      case 1:
+        Livro novoLivro = Livro.cadastrarLivro();
+        System.out.println("\nLivro cadastrado!\nResumo...\n");
+        System.out.println(novoLivro.toString());
+        livros.add(novoLivro);
+        break;
+      // cadastrar funcionario
+      // case 2:
+      // Funcionario novoFunc = Funcionario.cadastrarFuncionario();
+
+      default:
+        System.out.println("Saindo da func escolha");
+    }
+  }
+  //----------------------------------------------------------------
+  //----------------------------------------------------------------
+  //----------------------------------------------------------------
+
 }
